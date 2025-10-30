687dc5b9ddf6e7dd92b57b48a3fd9fd30c58248d
Aige <aigestudio@hotmail.com>
2025-10-30 23:17:09 +0800

    Migare to Maven Central.

--- WheelPicker/build.gradle.kts
+++ WheelPicker/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.vanniktech.maven.publish)
 }
 
 android {
@@ -45,4 +46,35 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.runtime)
+}
+
+mavenPublishing {
+    coordinates("dev.aige.pub", "WheelPicker", "1.2.0")
+
+    pom {
+        name.set("WheelPicker")
+        description.set("Simple and fantastic wheel view for Android, support Kotlin and Jetpack Compose.")
+        url.set("https://github.com/AigeStudio/WheelPicker")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("devaige")
+                name.set("AigeStudio")
+                email.set("aigestudio@qq.com")
+            }
+        }
+        scm {
+            connection.set("scm:git:github.com/AigeStudio/WheelPicker.git")
+            developerConnection.set("scm:git:ssh://github.com/AigeStudio/WheelPicker.git")
+            url.set("https://github.com/AigeStudio/WheelPicker/tree/master")
+        }
+    }
+
+    publishToMavenCentral(com.vanniktech.maven.publish.SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
 }
